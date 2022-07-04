81b81fb3b283e383e57857f455b080e17f6aae9b
AbayReushenov <abay.reushenov@gmail.com>
2022-07-04 16:48:38 +0300

    Add link

--- mobx/src/apps/CounterDisplay/CounterDisplay.tsx
+++ mobx/src/apps/CounterDisplay/CounterDisplay.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { Counter } from './Counter';
 
 type Props = {
@@ -11,19 +10,47 @@ type Props = {
     isNegative: string;
   };
   storeAsync: {
-    count: number
-  }
+    count: number;
+  };
 };
 
-export const CounterDisplay: React.FC<Props> = ({ state, store , storeAsync}) => {
+export const CounterDisplay: React.FC<Props> = ({
+  state,
+  store,
+  storeAsync,
+}) => {
   return (
     <div className='counter'>
       <div className='header'>
-        <h1>
-          Counters: <Counter state={state} title={"state is object: "} className={'red'} /> {`  <>  `}
-          <Counter className={'blue'}  title={"state is class: "} state={store} /> {`  <>  `}
-          <Counter className={'green'}  title={"asyncstate: "} state={storeAsync} />
+        <h1 className='title'>
+
+          <Counter
+            state={state}
+            title={'state is object: '}
+            className={'red'}
+          />{' '}
+          {`  <>  `}
+          <Counter
+            className={'blue'}
+            title={'state is class: '}
+            state={store}
+          />{' '}
+          {`  <>  `}
+          <Counter
+            className={'green'}
+            title={'asyncstate: '}
+            state={storeAsync}
+          />
         </h1>
+                  <a
+            className='link'
+            href='https://github.com/AbayReushenov/react-tutorial/tree/main/mobx'
+            target='_blank'
+            rel='noreferrer'
+            data-hover="Hello, this is the tooltip"
+          >
+            github.com/AbayReushenov
+          </a>
       </div>
     </div>
   );
